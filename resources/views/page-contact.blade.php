be80969563f9da18d871cdcf41f9ea735fa73735
tak4822 <tkc4822@gmail.com>
2018-05-24 01:06:40 -0700

    responsive contact page

--- resources/views/page-contact.blade.php
+++ resources/views/page-contact.blade.php
@@ -8,13 +8,18 @@
                 Contact
             @endslot
         @endcomponent
-
-        <p class="section-title">お問い合わせ</p>
-
-        <div class="page-contents">
-            @while(have_posts()) @php the_post() @endphp
-            @include('partials.content-page')
-            @endwhile
+        <div class="contents-wrapper">
+            <div class="contents-left">
+                <p class="section-title">お問い合わせ</p>
+                <div class="page-contents">
+                    @while(have_posts()) @php the_post() @endphp
+                    @include('partials.content-page')
+                    @endwhile
+                </div>
+            </div>
+            <div class="form">
+                <?php echo do_shortcode( '[contact-form-7 id="51" title="お問い合わせ"]' ); ?>
+            </div>
         </div>
     </section>
 @endsection
