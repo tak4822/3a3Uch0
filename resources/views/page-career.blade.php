2780c92bf12ac70c4bd6dad9dff0f725b2c24b46
tak4822 <tkc4822@gmail.com>
2018-05-24 00:52:29 -0700

    responsive career page

--- resources/views/page-career.blade.php
+++ resources/views/page-career.blade.php
@@ -8,29 +8,28 @@
                 Career
             @endslot
         @endcomponent
+        <div class="contents-wrapper">
+            <div class="contents-left">
+                <p class="section-title">採用情報</p>
 
-        <p class="section-title">採用情報</p>
+                <div class="page-contents">
+                    @while(have_posts()) @php the_post() @endphp
+                    @include('partials.content-page')
+                    @endwhile
+                </div>
+            </div>
 
-        <div class="page-contents">
-            @while(have_posts()) @php the_post() @endphp
-            @include('partials.content-page')
-            @endwhile
+            <div class="career-position-container">
+                @while($get_career->have_posts()) @php($get_career->the_post())
+
+                @component('partials.careerCard', [
+                    'positionName' => get_the_title(),
+                    'leadDesc' => get_post_meta( get_the_ID(), 'leadDesc', true ),
+                    'detail' => get_post_meta( get_the_ID(), 'detail', true ),
+                ])
+                @endcomponent
+                @endwhile
+            </div>
         </div>
-
-        {{--<pre>--}}
-        {{--{{ print_r($get_career) }}--}}
-        {{--</pre>--}}
-        <div class="career-position-container">
-            @while($get_career->have_posts()) @php($get_career->the_post())
-
-            @component('partials.careerCard', [
-                'positionName' => get_the_title(),
-                'leadDesc' => get_post_meta( get_the_ID(), 'leadDesc', true ),
-                'detail' => get_post_meta( get_the_ID(), 'detail', true ),
-            ])
-            @endcomponent
-            @endwhile
-        </div>
-
     </section>
 @endsection
