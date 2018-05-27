aa4fba713b7e0ced361ba2d66ca0cd418238df8f
tak4822 <tkc4822@gmail.com>
2018-05-27 00:57:22 -0700

    finish cardExpandAnimation

--- resources/assets/scripts/modules/careerCard.js
+++ resources/assets/scripts/modules/careerCard.js
@@ -1,6 +1,30 @@
+import { TweenLite, Expo, Power1 } from 'gsap/TweenMax';
+
 export default function() {
   $('.expandButton').on('click', function() {
-    $(this).siblings('.detail-container').css('display', 'block');
-    $(this).fadeOut(100);
+    const contents = $(this).siblings('.detail-container');
+    TweenLite.fromTo($(this), 0.3, {
+      alpha: 1,
+      filter: 'blur(0px)',
+      x: 0,
+    },{
+      alpha: 0,
+      filter: 'blur(5px)',
+      x: 10,
+      ease: Power1.easeIn,
+      onComplete: function() {
+        contents.show(500).css({'margin': '20px auto'});
+        this.target[0].style.display = 'none';
+        TweenLite.fromTo(contents, 1, {
+          alpha: 0,
+          y: 0,
+        },{
+          delay: 0.5,
+          alpha: 1,
+          y: 10,
+          ease: Expo.easeOut,
+        })
+      },
+    })
   })
 }
